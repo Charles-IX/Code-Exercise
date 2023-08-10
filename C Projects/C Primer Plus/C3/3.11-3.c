d2023697ebd4fbc2f6b25797dd68770d2d889235
Charles-IX <78650751+Charles-IX@users.noreply.github.com>
2023-08-10 21:35:49 +0800

    \a stands for bell ring in ANSI C

--- C Projects/C Primer Plus/C3/3.11-3.c
+++ C Projects/C Primer Plus/C3/3.11-3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
-    printf("\bStartled by the sudden sound, Charles wondered,\n");
+    printf("\aStartled by the sudden sound, Charles wondered,\n");
     printf("\"What the hell was that?\"");
     return 0;
 }
