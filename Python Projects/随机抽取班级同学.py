cfe7ea7bc806320f013dec3d7a9e7842522111e8
Charles-IX <78650751+Charles-IX@users.noreply.github.com>
2023-09-27 18:31:32 +0800

    the number of people wanted is now not a constant

--- Python Projects/随机抽取班级同学.py
+++ Python Projects/随机抽取班级同学.py
@@ -1,7 +1,9 @@
 import random
 
+number_str = input ("The number of people wanted:")
+number = int (number_str)
 FilePath = r"C:\Users\Charles\OneDrive\轻大文件\行软23-02\行软23-02 名单.txt"
 with open(FilePath, 'r', encoding='utf-8') as f:
     Namelist = [_.rstrip('\n') for _ in f.readlines()]
-LuckyOnes = random.sample(Namelist,k=28)
+LuckyOnes = random.sample(Namelist,k=number)
 print(LuckyOnes)
