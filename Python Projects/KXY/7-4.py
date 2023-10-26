cd6a366173afd06040f8b79ca509a26a46a4034d
Charles-IX <78650751+Charles-IX@users.noreply.github.com>
2023-10-26 23:03:24 +0800

    修正了一个铸币错误

--- Python Projects/KXY/7-4.py
+++ Python Projects/KXY/7-4.py
@@ -2,7 +2,9 @@ import math
 
 x = eval(input())
 
-y = math.sin(35) + (((math.e)**x - 15 * x) / (math.sqrt(x**4 + 1))) - math.log(7 * x, math.e)
+y = math.sin(math.radians(35)) + ((math.exp(x) - 15 * x) / (math.sqrt(x**4 + 1))) - math.log(7 * x)
+# 弧度制和角度制不能混着用，如果直接写 math.sin(35) 就会出现最终答案总是和标准答案差1.几的铸币错误
+# 同时，写 math.e**x 也非常粪，不如math.exp(x)，不过肯定比 math.e^x 这种脑残写法好不少（最后这种会报 unsupported operand type(s) for : 'float' and 'int'）
 
 print_y = round(y, 3)
 text = "f({})={}"
