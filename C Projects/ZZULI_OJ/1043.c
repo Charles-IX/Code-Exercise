8489811f549fb206ce49f97f39daccf6c924d189
Charles-IX <2429668770@qq.com>
2023-10-27 19:08:29 +0800

    我是铸币

--- C Projects/ZZULI_OJ/1043.c
+++ C Projects/ZZULI_OJ/1043.c
@@ -2,31 +2,21 @@
 // 输入有两行：第一行是一个正整数n，第二行是n个整数。
 // 输出包含一个整数，即n个数中的最大值，单独占一行。
 #include <stdio.h>
-int max(int x, int y);
 
 int main(void){
-    int n, set[n];
+    int n, temp;
     scanf("%d", &n);
+    int set[n];
     for (int i = 0; i < n; i++){
         scanf("%d", &set[i]);
     }
-    for (int i = 0; i < n; i++){
-        printf("%d ", &set[i]);
-    }
-    printf("%d\n", set[n]);
     for (int i = 0; i + 1 < n; i++){
-        max(set[i], set[i+1]);
+        if (set[i] > set[i + 1]){
+            temp = set[i];
+            set[i] = set[i + 1];
+            set[i + 1] = temp;
+        }
     }
-    printf("%d", set[n]);
+    printf("%d", set[n - 1]);
     return 0;
-
-}
-
-int max(int x, int y){
-    int temp;
-    if (x > y){
-        temp = x;
-        x = y;
-        y = temp;
-    }
 }
