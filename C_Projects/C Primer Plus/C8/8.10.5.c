e15fdb3811346b08427d20130df1a3248ad8578d
Charles-IX <78650751+Charles-IX@users.noreply.github.com>
2024-02-02 17:18:12 +0800

    需要后续修改

--- C_Projects/C Primer Plus/C8/8.10.5.c
+++ C_Projects/C Primer Plus/C8/8.10.5.c
@@ -2,32 +2,42 @@
 // 每次猜到后会问用户是猜大了还是猜小了还是刚刚好，通过二分法最终找到答案。
 #include <stdio.h>
 #include <string.h>
-int judge(char arr[], int n);
+int judge(char arr[], int *n, int *max, int *min);
 
 int main(void){
-    int n = 50;
-    int judge;
+    int n = 50, max = 100, min = 0;
+    int ans;
     char arr[10];
     printf("Pick an integer from 1 to 100. I will try to guess it.\n");
     printf("Tell me if my answer is too great or too small.\n");
-    printf("Is it 50?\n");
+    printf("Is it 50 ?\n");
     while (1){
         printf("Tell me, is my answer 'greater' or 'smaller' or 'hit'?\n");
         scanf("%s", arr);
-        judge(arr, n) = judge;
-        
-
-        
+        ans = judge(arr, &n, &max, &min);
+        if (ans == 0){
+            printf("I know it's %d !\n", ans);
+            break;
+        }
+        else if (ans == 114){
+            printf("I can only recognize 'greater', 'smaller', 'hit'.\n");
+            continue;
+        }
+        else{
+            printf("Well then, is it %d ?\n", ans);
+        }
     }
-    
+    return 0;
 }
 
-int judge(char arr[], int n){
+int judge(char arr[], int *n, int *max, int *min){
     if (strcmp(arr, "greater") == 0){
-        return (n + 100) / 2;
+        *max = (*max + *n) / 2;
+        return (*min + *n) / 2;
     }
     else if (strcmp(arr, "smaller") == 0){
-        return (100 - n) / 2;
+        *min = (*min + *n) / 2;
+        return (*max + *n) / 2;
     }
     else if (strcmp(arr, "hit") == 0){
         return 0;
